4aaf9825d83599d7753d91069b65d40d0f39712e
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-09-30 23:54:54 +0700

    feat(pages/podcast): pagination

--- src/app/views/pages/podcast/podcast_detail.php
+++ src/app/views/pages/podcast/podcast_detail.php
@@ -1,46 +1,46 @@
+<head>
+  <link rel="stylesheet" href="<?= CSS_DIR ?>/podcast/podcast_detail.css">
+</head>
 <div id="template">
   <div>
     <?php
         require_once VIEW_DIR . "components/podcast/contentBox.php";
-        // TODO: insert img url
+        require_once VIEW_DIR . "components/podcast/episodesList.php";
+        require_once VIEW_DIR . "components/podcast/paginationNav.php";
+
+        $MAX_EPS_PER_PAGE = 2;
 
         $podcast = $data['podcast'];
-        // $episodes = $data['$episodes'];
+        $episodes = $data['episodes'];
+        $total_pages = $podcast->total_eps == 0 ? 1 : ceil($podcast->total_eps / $MAX_EPS_PER_PAGE);
 
         // Podcasts view
         echo "<section class=\"podcast-detail-container\">";
             echo
             "
-                    <div class=\"podcast-detail\">
-                        <img src=\"\" alt=\"podcastImage\" class=\"podcast-detail-img\">
-                        <h1>$podcast->creator_name</h1>
-                        <h2>$podcast->total_eps episodes</h2>
-                        <h2>$podcast->description</h2>
-                    </div>
+                <div class=\"podcast-detail\">
+                    <img src=\"$podcast->image_url\" alt=\"podcastImage\" class=\"podcast-detail-img\">
+                    <h1>$podcast->creator_name</h1>
+                    <h2>$podcast->total_eps episodes</h2>
+                    <h2>$podcast->description</h2>
+                </div>
             ";
 
             echo
-            "<h3 class=\"episode-list-title\">Episodes</h3>
-                <div class=\"podcast-detail-eps-container\">
+            "   <h3 class=\"episode-list-title\">Episodes</h3>
+                <div class=\"podcast-detail-eps-container\" id=\"podcast-eps-list-container\">
             ";
                 if (count($episodes) > 0) {
-                    echo "<h2>No episodes in this podcast</h2>";
+                    episodeList($episodes);
                 }
                 else {
-                    foreach ($episodes as $episode) {
-                        echo
-                        "
-                            <div class=\"eps-row-content\">
-                                <img src=\"\" alt=\"episodeImage\" class=\"eps-img\">
-                                <div class=\"eps-data\">
-                                    <p class=\"eps-title\">$episode->title</p>
-                                    <p class=\"eps-desc\">$episode->description</p>
-                                </div>
-                            </div>
-                        ";
-                    }
+                    echo "<h2>No episodes in this podcast</h2>";
                 }
-        echo "</div></section>"
+            echo "</div>";
+
+            paginationEpsNav($podcast, $total_pages);
+
+        echo "</section>";
     ?>
   </div>
 </div>
---
+++ src/public/js/podcast/pagination.js
@@ -0,0 +1,40 @@
+/**
+ * Podcast Detail Page
+ */
+var pageNum = 1;
+
+const loadEpsList = (
+  podcastId,
+  next = true,
+  toEnd = false,
+  toStart = false
+) => {
+  var totalPages = document.getElementById("eps-list-total-pages").textContent;
+  if (next) {
+    if (next && pageNum < totalPages) {
+      pageNum++;
+      document.getElementById("eps-list-page-num").textContent = pageNum;
+    }
+  } else {
+    if (pageNum > 1) {
+      pageNum--;
+      document.getElementById("eps-list-page-num").textContent = pageNum;
+    }
+  }
+  
+  if (toEnd) {
+    pageNum = totalPages;
+  }
+  if (toStart) {
+    pageNum = 1;
+  }
+  document.getElementById("eps-list-page-num").textContent = pageNum;
+
+  // Load eps list from the server
+  const URL = "/episodes/" + podcastId + "?page=" + pageNum;
+  let xhttp = get(URL);
+  xhttp.onload = () => {
+    document.getElementById("podcast-eps-list-container").innerHTML =
+      xhttp.responseText;
+  };
+};
