e0b7a0b4992ef1b1eceaf3b58a55bd674a8f5904
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-09-30 17:12:33 +0700

    fix(components/podcast): add on click event for content box

--- src/app/views/components/podcast/contentBox.php
+++ src/app/views/components/podcast/contentBox.php
@@ -1,18 +1,24 @@
 <?php
 // Podcast box component
-function baseContentBox($podcast = null, $is_skeleton = false) {
+function baseContentBox($podcast = null, $is_skeleton = false, $click_evt = "") {
     $title = $podcast ? $podcast->title : '';
     $creator_name = $podcast ? $podcast->creator_name : '';
     $content_box_class = $is_skeleton ? 'podcast-content-skeleton' : 'base-content-box';
 
-    echo "
-      <div class=\"podcast-content-container\">
-        <div class=\"{$content_box_class}\">
-          <img src=\"\" alt=\"podcastImage\">
+    echo
+    "
+        <div class=\"podcast-content-container\" onclick=\"$click_evt\">
+            <div class=\"{$content_box_class}\">
+    ";
+    if (!$is_skeleton) {
+        echo"<img src=\"\" alt=\"podcastImage\">";
+    }
+    echo
+    "
+            </div>
+            <p class=\"podcast-title\">{$title}</p>
+            <br />
+            <p class=\"podcast-creator\">{$creator_name}</p>
         </div>
-        <p class=\"podcast-title\">{$title}</p>
-        <br />
-        <p class=\"podcast-creator\">{$creator_name}</p>
-      </div>
     ";
 }
