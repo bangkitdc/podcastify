4d22c317e86dcc59eff9ce8376b3bbe6623b3ea9
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-01 15:25:49 +0700

    feat: add upload service

--- src/app/controllers/PodcastController.php
+++ src/app/controllers/PodcastController.php
@@ -1,6 +1,7 @@
 <?php
-require_once BASE_URL . '/src/app/services/podcast/PodcastService.php';
-require_once BASE_URL . '/src/app/views/components/podcast/episodesList.php';
+require_once SERVICES_DIR . "/podcast/PodcastService.php";
+require_once SERVICES_DIR . "/upload/UploadService.php";
+require_once BASE_URL . "/src/app/views/components/podcast/episodesList.php";
 
 class PodcastController extends BaseController
 {
@@ -12,18 +13,21 @@ class PodcastController extends BaseController
 
     public function index()
     {
-        switch ($_SERVER['REQUEST_METHOD']) {
+        switch ($_SERVER["REQUEST_METHOD"]) {
             case "GET":
-                $isAjax = isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
+                $isAjax = isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && $_SERVER["HTTP_X_REQUESTED_WITH"] === "XMLHttpRequest";
 
                 $podcasts = $this->podcast_service->getAllPodcast();
 
+                $data["podcasts"] = $podcasts;
+
                 if ($isAjax) {
-                    $this->view("pages/podcast/index", ['podcasts' => $podcasts]);
+                    $this->view("pages/podcast/index", $data);
                 } else {
-                    // If it's not an AJAX request, include the full HTML structure
-                    $this->view('layouts/default', ['podcasts' => $podcasts]);
+                    // If it"s not an AJAX request, include the full HTML structure
+                    $this->view("layouts/default", $data);
                 }
+                break;
 
             case "POST":
                 return;
@@ -38,8 +42,8 @@ class PodcastController extends BaseController
     public function podcast($id) {
         $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
 
-        $data['podcast'] = $this->podcast_service->getPodcastById($id);
-        $data['episodes'] = $this->podcast_service->getEpisodesByPodcastId($id, 2, 1);
+        $data["podcast"] = $this->podcast_service->getPodcastById($id);
+        $data["episodes"] = $this->podcast_service->getEpisodesByPodcastId($id, 2, 1);
 
         $this->view("pages/podcast/podcast_detail", $data);
     }
@@ -47,16 +51,16 @@ class PodcastController extends BaseController
     // /episodes?page=
     public function episodes($id) {
         $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-        $page = isset($_GET['page']) ? filter_var($_GET['page'], FILTER_SANITIZE_STRING) : 1;
+        $page = isset($_GET["page"]) ? filter_var($_GET["page"], FILTER_SANITIZE_STRING) : 1;
 
-        $data['episodes'] = $this->podcast_service->getEpisodesByPodcastId($id, 2, $page);
+        $data["episodes"] = $this->podcast_service->getEpisodesByPodcastId($id, 2, $page);
 
-        return episodeList($data['episodes']);
+        return episodeList($data["episodes"]);
     }
 
     // /search?key=
     public function search() {
-        $search_key = isset($_GET['key']) ? filter_var($_GET['key'], FILTER_SANITIZE_STRING) : '';
+        $search_key = isset($_GET["key"]) ? filter_var($_GET["key"], FILTER_SANITIZE_STRING) : "";
 
         $podcasts = $this->podcast_service->getPodcastBySearch($search_key);
 
@@ -64,13 +68,43 @@ class PodcastController extends BaseController
     }
 
     public function edit($id) {
-        switch ($_SERVER['REQUEST_METHOD']) {
-            case 'GET':
+        switch ($_SERVER["REQUEST_METHOD"]) {
+            case "GET":
                 $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
 
-                $data['podcast'] = $this->podcast_service->getPodcastById($id);
+                $data["podcast"] = $this->podcast_service->getPodcastById($id);
+                $data["type"] = "edit";
 
                 $this->view("pages/podcast/podcast_management", $data);
+                break;
+            case "PATCH":
+
+
+                break;
+        }
+    }
+
+    public function create($id) {
+        switch ($_SERVER["REQUEST_METHOD"]) {
+            case "GET":
+                $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+                $data["type"] = "create";
+
+                $this->view("pages/podcast/podcast_management", $data);
+                break;
+            case "POST":
+                break;
+        }
+    }
+
+    public function upload() {
+        switch ($_SERVER["REQUEST_METHOD"]) {
+            case "POST":
+                UploadService::upload();
+                break;
+            default:
+                ResponseHelper::responseNotAllowedMethod();
                 break;
         }
     }
