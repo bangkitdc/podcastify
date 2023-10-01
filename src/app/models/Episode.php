b5381d67cee9e71e2e6ff2b64929a2335244b634
irsyadnb <irsyadxd@gmail.com>
2023-10-02 01:36:58 +0700

    chore : CRUD episode model

--- src/app/models/Episode.php
+++ src/app/models/Episode.php
@@ -26,7 +26,7 @@ class Episode {
   }
 
   public function findAllEpisodeCard($page = 1, $limit = 10) {
-    $query = "SELECT episodes.title, podcasts.creator_name, episodes.duration, episodes.image_url FROM episodes, podcasts WHERE episodes.podcast_id = podcasts.podcast_id LIMIT $limit OFFSET :offset";
+    $query = "SELECT episodes.episode_id, episodes.title, podcasts.creator_name, episodes.duration, episodes.image_url FROM episodes, podcasts WHERE episodes.podcast_id = podcasts.podcast_id LIMIT $limit OFFSET :offset";
     $this->db->query($query);
 
     $offset = ($page - 1) * $limit;
@@ -37,10 +37,47 @@ class Episode {
   }
 
   public function findByIdEpisodeDetail($episode_id) {
-    $query = "SELECT episodes.image_url, episodes.title, episodes.duration, episodes.created_at, episodes.description, podcasts.image_url AS creator_img, podcasts.creator_name FROM episodes, podcasts WHERE episodes.podcast_id = podcasts.podcast_id and episode_id = $episode_id";
+    $query = "SELECT episodes.episode_id, episodes.image_url, episodes.title, episodes.duration, episodes.created_at, episodes.description, podcasts.image_url AS creator_img, podcasts.creator_name FROM episodes, podcasts WHERE episodes.podcast_id = podcasts.podcast_id and episode_id = $episode_id";
     $this->db->query($query);
     $result = $this->db->fetchAll();
     return $result;
+  }
+
+  public function createEpisode($podcast_id, $category_id, $title, $description, $duration, $image_url, $audio_url) {
+    $query = "INSERT INTO `episodes` (`podcast_id`, `category_id`,`title`,`description`, `duration`,`image_url`, `audio_url`) VALUES (:podcast_id, :category_id, :title, :description, :duration, :image_url, :audio_url)";
+
+    $this->db->query(($query));
+
+    var_dump($title);
+    var_dump($description);
+
+    $this->db->bind(":podcast_id", $podcast_id);
+    $this->db->bind(":category_id", $category_id);
+    $this->db->bind(":title", $title);
+    $this->db->bind(":description", $description);
+    $this->db->bind(":duration", $duration);
+    $this->db->bind(":image_url", $image_url);
+    $this->db->bind(":audio_url", $audio_url);
+
+    $this->db->execute();
+  }
+
+  public function deleteEpisode($episode_id) {
+    $query = "DELETE FROM episodes WHERE episodes.episode_id = :episode_id";
+
+    $this->db->query($query);
+
+    $this->db->bind(":episode _id", $episode_id);
+
+    $this->db->execute();
+  }
+
+  public function updateEpisode($episode_id, $title, $description, $image_url, $audio_url) {
+    $query = "UPDATE episodes SET title = :title, description = :description, creator_name = :creator_name, image_url = :image_url, updated_at = NOW() WHERE episode_id = :episode_id";
+
+    $this->db->query($query);
+
+    
   }
 
 }
