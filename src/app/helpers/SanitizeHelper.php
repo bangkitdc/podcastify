cde4a68bd92be20045edf666df2150a36ccdad31
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-04 00:23:25 +0700

    chore: add sanitizer for int

--- src/app/helpers/SanitizeHelper.php
+++ src/app/helpers/SanitizeHelper.php
@@ -1,11 +1,15 @@
 <?php
 
 class Sanitizer {
-    public static function sanitizeParam($param, $default = "") {
+    public static function sanitizeStringParam($param, $default = "") {
         return isset($_GET[$param]) ? filter_var($_GET[$param], FILTER_SANITIZE_STRING) : $default;
     }
 
-    public static function sanitizeArrayParam($param, $default = []) {
+    public static function sanitizeIntParam($param, $default = 1) {
+        return isset($_GET[$param]) ? filter_var($_GET[$param], FILTER_SANITIZE_NUMBER_INT) : $default;
+    }
+
+    public static function sanitizeStringArrayParam($param, $default = []) {
         return isset($_GET[$param]) ? array_map('filter_var', $_GET[$param], array_fill(0, count($_GET[$param]), FILTER_SANITIZE_STRING)) : $default;
     }
 }
