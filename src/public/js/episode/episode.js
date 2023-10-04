95a91bd7f337c0945b800dc10646f9c6b4f38d9e
irsyadnb <irsyadxd@gmail.com>
2023-10-04 19:47:22 +0700

    chore: styling and fix routing bugs

--- src/public/js/episode/episode.js
+++ src/public/js/episode/episode.js
@@ -1,27 +1,31 @@
-const EPISODE_BASE_URL = "/episode";
+const EPISODE_BASE_URL = "/episode/";
 
-const getEpisode = (url, async = true) => {
+const getEpisode = (url) => {
   let xhr = new XMLHttpRequest();
 
-  xhr.open('GET', EPISODE_BASE_URL + url, async);
+  xhr.open('GET', EPISODE_BASE_URL + url, true);
   xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
   xhr.send();
 
   return xhr;
 }
 
-const showEpisode = (episodeId) => {
-  const url = "/episode/" + episodeId;
-  let xhr = getEpisode(url);
+// const showEpisode = (episodeId) => {
+//   const url = "/episode/" + episodeId;
+//   let xhr = getEpisode(url);
 
-  location.replace(url);
+//   location.replace(url);
+
+//   xhr.onload = () => {
+//     document.getElementById("main-content").innerHTML = xhr.responseText;
+//   };
+// }
+
+const showEditEpisode = (episodeId) => {
+  const url = episodeId + "?edit=true";
+  let xhr = getEpisode(url);
 
   xhr.onload = () => {
     document.getElementById("main-content").innerHTML = xhr.responseText;
   };
 }
-
-// const showEditEpisode = (e) => {
-//   e.stopPropagation();
-//   window.location.href = "/episode/edit/?=true";
-// }
