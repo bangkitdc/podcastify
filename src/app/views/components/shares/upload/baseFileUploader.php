dd29814408d066e39c7cfb57f644088875fe9969
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-10-05 14:33:18 +0700

    fix: limit baseFileUploader input

--- src/app/views/components/shares/upload/baseFileUploader.php
+++ src/app/views/components/shares/upload/baseFileUploader.php
@@ -5,10 +5,11 @@
 
 function baseImageUploader($input_id = "", $input_class = "", $img_id = "", $is_required = true, $img_class = "") {
     $required_status = $is_required ? 'required' : '';
+    $allowed_img = implode(',', array_keys(ALLOWED_IMAGES));
     echo
     "
     <div class=\"file-uploader-container\">
-        <input type=\"file\" id=\"$input_id\" class=\"file-uploader $input_class\" $required_status name=\"$input_id\" />
+        <input type=\"file\" id=\"$input_id\" class=\"file-uploader $input_class\" $required_status name=\"$input_id\" accept=\"$allowed_img\" />
         <label for=\"$input_id\" class=\"file-uploader-label\">
             Choose File
         </label>
@@ -20,10 +21,11 @@ function baseImageUploader($input_id = "", $input_class = "", $img_id = "", $is_
 
 function baseAudioUploader($input_id = "", $input_class = "", $is_required = true) {
     $required_status = $is_required ? 'required' : '';
+    $allowed_audio = implode(',', array_keys(ALLOWED_AUDIOS));
     echo
     "
     <div class=\"file-uploader-container\">
-        <input type=\"file\" id=\"$input_id\" class=\"file-uploader $input_class\" $required_status name=\"$input_id\" />
+        <input type=\"file\" id=\"$input_id\" class=\"file-uploader $input_class\" $required_status name=\"$input_id\" accept=\"$allowed_audio\" />
         <label for=\"$input_id\" class=\"file-uploader-label\">
             Choose File
         </label>
