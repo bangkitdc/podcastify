497955afc000b8f2aa83ad103f3903c2182d7a35
irsyadnb <irsyadxd@gmail.com>
2023-10-06 21:05:32 +0700

    styles: audio player styling

--- src/app/views/components/shares/navbars/media_player.php
+++ src/app/views/components/shares/navbars/media_player.php
@@ -4,7 +4,7 @@ echo '<link rel="stylesheet" href="' . CSS_DIR . 'navbars/media_player.css">';
 
 echo '
   <div class="media-player">
-    <div class="media-cover">
+    <div id="media-cover" class="media-cover">
       
     </div>
     <div class="player">
@@ -15,13 +15,16 @@ echo '
         </button>
       </div>
       <div class="player-timeline">
-        <span class="current-time">0:00</span>
+        <span id="slider-current-time" class="current-time">-:--</span>
         <input id="range" type="range" class="timeline-slider" min="0" max="100" step="1" value="0"/>
-        <span class="total-time">0:00</span>
+        <span id="slider-total-time" class="total-time">-:--</span>
       </div>
     </div>
     <div class="volume-controls">
-      
+      <button id="mute-button">
+      <img id="mute-button-img" src="' . ICONS_DIR . 'volume.svg" />
+      </button>
+      <input id="volume-slider" type="range" min="0" max="1" step="0.01" value="1">
     </div>
   </div>
   <script src="' . JS_DIR . 'episode/media_player.js"></script>
